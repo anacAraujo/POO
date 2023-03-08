6b28e5f0e373101a5cdd7dc5fa78d0db79564ceb
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-03-08 22:15:58 +0000

    Add toString

--- src/aula04/CashRegisterDemo.java
+++ src/aula04/CashRegisterDemo.java
@@ -27,21 +27,28 @@ class Product {
         return quantity;
     }
 
+    public String toString() {
+        return String.format("%10s %10.2f %10d %10.2f\n",
+                this.name,
+                this.price,
+                this.quantity,
+                this.price * this.quantity);
+    }
 }
 
 class CashRegister {
 
-    float valorTotal = 0;
-    Product[] carrinho = new Product[5];
-    int qntProdutos = 0;
+    private Product[] carrinho;
+    private int qntProdutos;
 
-    public float addProduct(Product novoProduto) {
-        valorTotal += novoProduto.getPrice() * novoProduto.getQuantity();
+    public CashRegister() {
+        this.carrinho = new Product[5];
+        this.qntProdutos = 0;
+    }
 
+    public void addProduct(Product novoProduto) {
         carrinho[qntProdutos] = novoProduto;
         qntProdutos++;
-
-        return valorTotal;
     }
 
     public String toString() {
@@ -49,14 +56,10 @@ class CashRegister {
         float totalValue = 0;
 
         for (int i = 0; i < carrinho.length; i++) {
-            imprimir += String.format("%10s %10.2f %10d %10.2f\n",
-                    carrinho[i].getName(),
-                    carrinho[i].getPrice(),
-                    carrinho[i].getQuantity(),
-                    carrinho[i].getPrice() * carrinho[i].getQuantity());
+            imprimir += carrinho[i].toString();
             totalValue += carrinho[i].getPrice() * carrinho[i].getQuantity();
         }
-        imprimir += String.format("%9s %7.2f", "Total value: ", totalValue);
+        imprimir += String.format("\n%9s %7.2f", "Total value: ", totalValue);
         return imprimir;
     }
 
