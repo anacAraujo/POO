53c675ab36871eb5622696b2462956af0b6ef7db
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-03-08 13:05:23 +0000

    Add first lines to registerCars

--- src/aula04/CarDemo.java
+++ src/aula04/CarDemo.java
@@ -2,13 +2,14 @@ package aula04;
 
 import java.util.Scanner;
 
-class Car {
+// Originalmente a class CarA tinha a designação Car que foi mudada por fazer interferência com a class Car do ficheiro SimpleCarDemo
+class CarA {
     public String make;
     public String model;
     public int year;
     public int kms;
 
-    public Car(String make, String model, int year, int kms) {
+    public CarA(String make, String model, int year, int kms) {
         this.make = make;
         this.model = model;
         this.year = year;
@@ -17,6 +18,7 @@ class Car {
 
     public void drive(int distance) {
         // TODO: acumular distância percorrida
+        System.out.println();
     }
 
 }
@@ -25,20 +27,34 @@ public class CarDemo {
 
     static Scanner sc = new Scanner(System.in);
 
-    static int registerCars(Car[] cars) {
+    static int registerCars(CarA[] cars) {
         // TODO: pede dados dos carros ao utilizador e acrescenta ao vetor
         // registo de carros termina quando o utilizador inserir uma linha vazia
         // devolve número de carros registados
-        System.out.print("Insira dados do carro (marca modelo ano quilómetros): ");
+        int numCarsUser = 0;
+        String carroUser = "a";
+        do {
+            System.out.print("Insira dados do carro (marca modelo ano quilómetros): ");
+            carroUser = sc.next();
+            String[] partesCarro = carroUser.split(" ");
+            String marca = partesCarro[0];
+            String modelo = partesCarro[1];
+            String ano = partesCarro[2];
+            String kms = partesCarro[3];
+            numCarsUser++;
+        } while (carroUser != "");
+
+        return numCarsUser;
+
     }
 
-    static void registerTrips(Car[] cars, int numCars) {
+    static void registerTrips(CarA[] cars, int numCars) {
         // TODO: pede dados das viagens ao utilizador e atualiza informação do carro
         // registo de viagens termina quando o utilizador inserir uma linha vazia
         System.out.print("Registe uma viagem no formato \"carro:distância\": ");
     }
 
-    static void listCars(Car[] cars) {
+    static void listCars(CarA[] cars) {
         System.out.println("\nCarros registados: ");
         // TODO: lista todos os carros registados
         // Exemplo de resultado
@@ -51,7 +67,7 @@ public class CarDemo {
 
     public static void main(String[] args) {
 
-        Car[] cars = new Car[10];
+        CarA[] cars = new CarA[10];
 
         int numCars = registerCars(cars);
 
