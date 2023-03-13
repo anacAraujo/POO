7bcca8287e109bbdbe7175eed8ee714feaed944e
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-03-13 13:47:39 +0000

    Add second method

--- src/aula05/DateYMD.java
+++ src/aula05/DateYMD.java
@@ -8,4 +8,17 @@ public class DateYMD {
         }
         return false;
     }
+
+    public int monthDays(int month, int year) {
+        int totalMonthDays;
+        if (month == 4 || month == 6 || month == 9 || month == 11) {
+            totalMonthDays = 30;
+        } else if (month == 2) {
+            totalMonthDays = (month % 4 == 0) ? 29 : 28;
+        } else {
+            totalMonthDays = 31;
+        }
+        return totalMonthDays;
+    }
+
 }
