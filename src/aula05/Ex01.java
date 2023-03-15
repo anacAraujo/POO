726b43da5e88b33528aa6a82132b59bdd9573339
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-03-15 23:51:16 +0000

    Improve Ex1

--- src/aula05/Ex01.java
+++ src/aula05/Ex01.java
@@ -18,7 +18,6 @@ class DateYMD {
         this.year = year;
     }
 
-    // o set não era suposto ser individual para cada atributo?
     public void setDate(int day, int month, int year) {
         if (!valid(day, month, year)) {
             System.out.println("Invalid date.");
@@ -45,33 +44,33 @@ class DateYMD {
         return this.year + "-" + this.month + "-" + this.day;
     }
 
-    public boolean validMonth(int month) {
+    private static boolean validMonth(int month) {
         if (month >= 1 && month <= 12) {
             return true;
         }
         return false;
     }
 
-    public int monthDays(int month, int year) {
+    public static int monthDays(int month, int year) {
         int totalMonthDays;
         if (month == 4 || month == 6 || month == 9 || month == 11) {
             totalMonthDays = 30;
         } else if (month == 2) {
-            totalMonthDays = (year % 4 == 0) ? 29 : 28;
+            totalMonthDays = leapYear(year) ? 29 : 28;
         } else {
             totalMonthDays = 31;
         }
         return totalMonthDays;
     }
 
-    public boolean leapYear(int year) {
+    private static boolean leapYear(int year) {
         if (year % 4 == 0) {
             return true;
         }
         return false;
     }
 
-    public boolean valid(int day, int month, int year) {
+    public static boolean valid(int day, int month, int year) {
         if (day > monthDays(month, year) || day <= 0
                 || !validMonth(month) || year < 1000) {
             return false;
@@ -107,14 +106,14 @@ class DateYMD {
 
 public class Ex01 {
     public static void main(String[] args) {
-        // TODO menu
+
         Scanner sc = new Scanner(System.in);
 
         int userOption;
-        int userDay = 0;
-        int userMonth = 0;
-        int userYear = 0;
-        DateYMD userDateYMD = new DateYMD(userDay, userMonth, userYear);
+
+        DateYMD userDateYMD = null;
+
+        boolean isValid = false;
 
         do {
             System.out.println(
@@ -124,33 +123,39 @@ public class Ex01 {
                 case 1:
                     do {
                         System.out.print("Enter a year: ");
-                        userYear = sc.nextInt();
+                        int userYear = sc.nextInt();
 
                         System.out.print("Enter a month: ");
-                        userMonth = sc.nextInt();
+                        int userMonth = sc.nextInt();
 
                         System.out.print("Enter a day: ");
-                        userDay = sc.nextInt();
+                        int userDay = sc.nextInt();
 
-                        userDateYMD = new DateYMD(userDay, userMonth, userYear);
-                        break;
-                    } while (!userDateYMD.valid(userDay, userMonth, userYear));
+                        isValid = DateYMD.valid(userDay, userMonth, userYear);
+
+                        if (isValid) {
+                            userDateYMD = new DateYMD(userDay, userMonth, userYear);
+                        } else {
+                            System.out.println("Invalid date. Enter again.");
+                        }
+                    } while (!isValid);
+                    break;
                 case 2:
-                    if (!userDateYMD.valid(userDay, userMonth, userYear)) {
+                    if (userDateYMD == null) {
                         System.out.println("Enter a valid date first.");
                         break;
                     }
                     System.out.println("Current date: " + userDateYMD);
                     break;
                 case 3:
-                    if (!userDateYMD.valid(userDay, userMonth, userYear)) {
+                    if (userDateYMD == null) {
                         System.out.println("Enter a valid date first.");
                         break;
                     }
                     userDateYMD.increment();
                     break;
                 case 4:
-                    if (!userDateYMD.valid(userDay, userMonth, userYear)) {
+                    if (userDateYMD == null) {
                         System.out.println("Enter a valid date first.");
                         break;
                     }
