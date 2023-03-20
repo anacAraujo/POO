b117e70153642f9621cf200d585a956530abdafb
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-03-20 21:49:53 +0000

    Add validation

--- src/aula06/Ex1.java
+++ src/aula06/Ex1.java
@@ -9,12 +9,24 @@ class Pessoa {
 
     public Pessoa(String nome, int cc, DateYMD dataNasc) {
         if (!DateYMD.valid(dataNasc.getDay(), dataNasc.getMonth(), dataNasc.getYear())) {
-            System.out.println("Invalid date.");
+            System.out.println("Data invalida.");
+            return;
+        }
+        if (!validPessoa(nome, cc)) {
+            System.out.println("Dados invalidos.");
             return;
         }
         this.nome = nome;
         this.cc = cc;
         this.dataNasc = dataNasc;
+    }
+
+    public static boolean validPessoa(String nome, int numcc) {
+        String cc = Integer.toString(numcc);
+        if (nome != "" || nome.matches("[^0-9]") || cc.matches("^[0-9]{8}$)")) {
+            return true;
+        }
+        return false;
     }
 
 }
