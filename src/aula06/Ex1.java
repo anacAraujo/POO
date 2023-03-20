c43fad14c5a6a50d29368fa970088ec5d17d1f39
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-03-20 21:29:47 +0000

    Add constructor

--- src/aula06/Ex1.java
+++ src/aula06/Ex1.java
@@ -6,6 +6,17 @@ class Pessoa {
     private String nome;
     private int cc;
     private DateYMD dataNasc;
+
+    public Pessoa(String nome, int cc, DateYMD dataNasc) {
+        if (!DateYMD.valid(dataNasc.getDay(), dataNasc.getMonth(), dataNasc.getYear())) {
+            System.out.println("Invalid date.");
+            return;
+        }
+        this.nome = nome;
+        this.cc = cc;
+        this.dataNasc = dataNasc;
+    }
+
 }
 
 public class Ex1 {
