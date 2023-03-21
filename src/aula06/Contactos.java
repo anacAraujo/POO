d30e5258aa8e72730250737136722b9440b605ab
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-03-21 16:06:00 +0000

    Add class Contactos

---
+++ src/aula06/Contactos.java
@@ -0,0 +1,10 @@
+package aula06;
+
+public class Contactos {
+    private static int currentID = 1;
+
+    private int id;
+    private int numTelemovel;
+    private String email;
+    private Pessoa pessoa;
+}
