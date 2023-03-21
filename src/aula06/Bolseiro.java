d0b2ff672fdc56b12bd00b3b2f4bbc55c29b2f56
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-03-21 12:13:22 +0000

    Add set and get methods

--- src/aula06/Bolseiro.java
+++ src/aula06/Bolseiro.java
@@ -3,12 +3,25 @@ package aula06;
 import aula05.DateYMD;
 
 public class Bolseiro extends Aluno {
-    private String orientador;
+    private Professor orientador;
     private int montante;
 
-    public Bolseiro(String nome, int cc, DateYMD dataNasc, DateYMD dataInsc, String orientador, int montante) {
-        super(nome, cc, dataNasc, dataInsc);
+    public Bolseiro(String nome, int cc, DateYMD dataNasc, Professor orientador, int montante) {
+        super(nome, cc, dataNasc);
         this.orientador = orientador;
         this.montante = montante;
     }
+
+    public Professor getOrientador() {
+        return orientador;
+    }
+
+    public int getBolsa() {
+        return montante;
+    }
+
+    public void setBolsa(int montante) {
+        this.montante = montante;
+    }
+
 }
