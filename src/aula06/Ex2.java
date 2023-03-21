7ded9c42e06b5bce8f50648b7f399230aeec11b2
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-03-21 19:53:13 +0000

    Change class

---
+++ src/aula06/Ex2.java
@@ -0,0 +1,75 @@
+package aula06;
+
+import aula05.DateYMD;
+import java.util.Scanner;
+
+class Contacto {
+    private static int currentID = 1;
+
+    private int id;
+    private int numTelemovel;
+    private String email;
+    private Pessoa pessoa;
+
+    public Contacto(int numTelemovel, String email, Pessoa pessoa) {
+        this.id = currentID;
+        this.numTelemovel = numTelemovel;
+        this.email = email;
+        this.pessoa = pessoa;
+        Contacto.currentID++;
+    }
+
+    public boolean validContact(int numTelemovel) {
+        String numTele = Integer.toString(numTelemovel);
+        if (numTele.matches("\\d{9}") && numTele.matches("\\^9")) {
+            return true;
+        }
+        return false;
+    }
+}
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int inputUser = 0;
+
+        do {
+            System.out.println(
+                    "1 - Inserir contacto;\n2 - Alterar contacto;\n3 - Apagar contacto;\n4 - Procurar contacto\n5 - Procurar contacto\n0 - Sair");
+            inputUser = sc.nextInt();
+            switch (inputUser) {
+                case 1:
+                    System.out.print("Nome: ");
+                    String nome = sc.nextLine();
+
+                    System.out.print("CC: ");
+                    int cc = sc.nextInt();
+
+                    System.out.print("Data nascimento: ");
+                    String data = sc.nextLine();
+                    String[] elementosData = data.split("-");
+                    int day = Integer.parseInt(elementosData[0]);
+                    int month = Integer.parseInt(elementosData[1]);
+                    int year = Integer.parseInt(elementosData[2]);
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                default:
+                    break;
+            }
+        } while (inputUser != 0);
+
+        sc.close();
+    }
+}
