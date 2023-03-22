7ff30c05a16a6fc0871087890b0ea48f2c546945
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-03-22 11:56:44 +0000

    Add validation

--- src/aula06/Ex2.java
+++ src/aula06/Ex2.java
@@ -5,26 +5,58 @@ import java.util.Scanner;
 
 class Contacto {
     private static int currentID = 1;
+    private static Pessoa[] pessoas;
 
     private int id;
-    private int numTelemovel;
+    private String numTelemovel;
     private String email;
     private Pessoa pessoa;
 
-    public Contacto(int numTelemovel, String email, Pessoa pessoa) {
+    public Contacto(String numTelemovel, String email, Pessoa pessoa) {
         this.id = currentID;
         this.numTelemovel = numTelemovel;
         this.email = email;
         this.pessoa = pessoa;
+
+        Contacto.pessoas[currentID - 1] = this.pessoa;
         Contacto.currentID++;
     }
 
-    public static boolean validContact(int numTelemovel) {
-        String numTele = Integer.toString(numTelemovel);
-        if (numTele.matches("\\d{9}") && numTele.matches("\\^9")) {
+    public static boolean validContact(String numTelemovel) {
+        if (numTelemovel.matches("[9]+\\d{8}") || numTelemovel == null) {
             return true;
         }
         return false;
+    }
+
+    public static boolean validEmail(String email) {
+        if (email.matches("[a-z]+[@]{1}[a-z]+[.]{1}.+") || email == null) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean pessoaUnica(String nome, int cc, DateYMD data) {
+        if (Contacto.pessoas == null) {
+            return true;
+        }
+        for (int i = 0; i < pessoas.length; i++) {
+            Pessoa pessoaNova = new Pessoa(nome, cc, data);
+            if (pessoas[i].equals(pessoaNova)) {
+                return true;
+            }
+        }
+        // for (Pessoa pessoa : pessoas) {
+        // Pessoa pessoaNova = new Pessoa(nome, cc, data);
+        // if (pessoa.equals(pessoaNova)) {
+        // return true;
+        // }
+        // }
+        return false;
+    }
+
+    public Pessoa[] getPessoas() {
+        return pessoas;
     }
 }
 
@@ -36,26 +68,37 @@ public class Ex2 {
 
         do {
             System.out.println(
-                    "1 - Inserir contacto;\n2 - Alterar contacto;\n3 - Apagar contacto;\n4 - Procurar contacto\n5 - Procurar contacto\n0 - Sair");
+                    "1 - Inserir contacto;\n2 - Alterar contacto;\n3 - Apagar contacto;\n4 - Procurar contacto\n5 - Listar contactos\n0 - Sair");
             inputUser = sc.nextInt();
             switch (inputUser) {
                 case 1:
                     System.out.print("Nome: ");
-                    String nome = sc.nextLine();
+                    String nome = sc.next();
 
                     System.out.print("CC: ");
                     int cc = sc.nextInt();
 
                     System.out.print("Data nascimento: ");
-                    String data = sc.nextLine();
+                    String data = sc.next();
                     String[] elementosData = data.split("-");
                     int day = Integer.parseInt(elementosData[0]);
                     int month = Integer.parseInt(elementosData[1]);
                     int year = Integer.parseInt(elementosData[2]);
                     DateYMD nascimento = new DateYMD(day, month, year);
 
+                    Boolean pessoaUnica = Contacto.pessoaUnica(nome, cc, nascimento);
+                    if (!pessoaUnica) {
+                        System.out.println(
+                                "Esta pessoa já existe. Quer continuar a inserir um novo contacto?\n 1 - sim\n2 - nao\n");
+                        inputUser = sc.nextInt();
+                        if (inputUser == 2) {
+                            break;
+                        }
+                    }
+                    Pessoa pessoa = new Pessoa(nome, cc, nascimento);
+
                     System.out.print("Numero telemovel: ");
-                    int numTele = sc.nextInt();
+                    String numTele = sc.next();
                     boolean valido = Contacto.validContact(numTele);
                     if (!valido) {
                         System.out.println("Numero invalido.");
@@ -63,9 +106,18 @@ public class Ex2 {
                     }
 
                     System.out.print("Email: ");
-                    String mail = sc.nextLine();
+                    String mail = sc.next();
+                    boolean emailValido = Contacto.validEmail(mail);
+                    if (!emailValido) {
+                        System.out.println("Email invalido.");
+                        break;
+                    }
 
-                    Pessoa pessoa = new Pessoa(nome, cc, nascimento);
+                    if (numTele == null && mail == null) {
+                        System.out.println("É necessário inserir pelo menos um número ou um email.");
+                        break;
+                    }
+
                     Contacto contacto = new Contacto(numTele, mail, pessoa);
                     break;
                 case 2:
