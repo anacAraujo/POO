c4d6cbc04fbcb70e1266c393830f5f8f31d3a567
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-03-22 21:59:40 +0000

    Fix constructor Bolseiro

--- src/aula06/Bolseiro.java
+++ src/aula06/Bolseiro.java
@@ -6,10 +6,14 @@ public class Bolseiro extends Aluno {
     private Professor orientador;
     private int montante;
 
-    public Bolseiro(String nome, int cc, DateYMD dataNasc, /* DateYMD dataInsc, */ Professor orientador, int montante) {
-        super(nome, cc, dataNasc/* , dataInsc */);
+    public Bolseiro(String nome, int cc, DateYMD dataNasc, DateYMD dataInsc, Professor orientador, int montante) {
+        super(nome, cc, dataNasc, dataInsc);
         this.orientador = orientador;
         this.montante = montante;
+    }
+
+    public Bolseiro(String nome, int cc, DateYMD dataNasc, Professor orientador, int montante) {
+        this(nome, cc, dataNasc, null, orientador, montante);
     }
 
     public Professor getOrientador() {
@@ -20,13 +24,17 @@ public class Bolseiro extends Aluno {
         return montante;
     }
 
+    public void setOrientador(Professor orientador) {
+        this.orientador = orientador;
+    }
+
     public void setBolsa(int montante) {
         this.montante = montante;
     }
 
     public String toString() {
         return String.format(
-                "%s; Orientador:%2s; Bolsa:%2d\n",
+                "%s; Orientador: %s; Bolsa: %d\n",
                 super.toString(),
                 this.orientador,
                 this.montante);
