881c9272718d2f6a9cb1522b3f1ff1e16d1ee4a8
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-03-28 18:35:06 +0100

    Fix Objeto and Robo

--- src/aula07/Ex03.java
+++ src/aula07/Ex03.java
@@ -6,12 +6,12 @@ import java.util.List;
 import java.util.Scanner;
 
 class Objeto {
-    private static int distPercorrida = 0;
-
+    private int distPercorrida;
     private int posX;
     private int posY;
 
     public Objeto(int posX, int posY) {
+        this.distPercorrida = 0;
         this.posX = posX;
         this.posY = posY;
     }
@@ -22,7 +22,7 @@ class Objeto {
 
         this.posX = newX;
         this.posY = newY;
-        Objeto.distPercorrida += Math.sqrt((this.posY - posAnteriorY) * (this.posY - posAnteriorY)
+        distPercorrida += Math.sqrt((this.posY - posAnteriorY) * (this.posY - posAnteriorY)
                 + (this.posX - posAnteriorX) * (this.posX - posAnteriorX));
     }
 
@@ -37,19 +37,21 @@ class Objeto {
 
 class Robo extends Objeto {
     private static List<String> categorias = Arrays.asList("GuardaRedes", "Avancado", "Defesa", "Medio");
+    private static int currentID = 0;
 
     private String id;
     private String tipoJogador;
     private int numGolos;
 
-    public Robo(String id, int posX, int posY, String tipo) {
+    public Robo(int posX, int posY, String tipo) {
         super(posX, posY);
         if (categorias.indexOf(tipo) == -1) {
             System.out.println("Tipo de jogador invalido.");
             return;
         }
         this.tipoJogador = tipo;
-        this.id = id;
+        this.id = "Robo" + currentID;
+        currentID++;
         numGolos = 0;
     }
 
@@ -92,35 +94,21 @@ class Bola extends Objeto {
 }
 
 class Equipa {
-    private static Scanner sc = new Scanner(System.in);
-
     private String nomeEquipa;
     private String nomeResponsavel;
     private int golosMarcados;
     private int golosSofridos;
     private Robo[] robos;
 
-    public Equipa(String nomeEquipa, String nomeResponsavel) {
+    public Equipa(String nomeEquipa, String nomeResponsavel, int posX, int posY, String tipo) {
         this.nomeEquipa = nomeEquipa;
         this.nomeResponsavel = nomeResponsavel;
         this.golosMarcados = 0;
         this.golosSofridos = 0;
         this.robos = new Robo[3];
-        adicionarRobos();
-    }
 
-    private void adicionarRobos() {
         for (int i = 0; i < robos.length; i++) {
-            System.out.print("ID do robô: ");
-            String id = sc.next();
-            System.out.print("Posição x: ");
-            int posX = sc.nextInt();
-            System.out.print("Posição Y: ");
-            int posY = sc.nextInt();
-            System.out.print("Tipo: ");
-            String tipo = sc.next();
-
-            robos[i] = new Robo(id, posX, posY, tipo);
+            robos[i] = new Robo(posX, posY, tipo);
         }
     }
 
@@ -141,6 +129,10 @@ class Equipa {
                 this.golosSofridos,
                 this.robos.toString());
     }
+}
+
+class Jogo {
+
 }
 
 public class Ex03 {
