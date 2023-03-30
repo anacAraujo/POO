f05c4523feb436252968d1836b15a52e0294ad5b
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-03-30 22:59:58 +0100

    Add extends Date and remove static methods

--- src/aula07/DateYMD.java
+++ src/aula07/DateYMD.java
@@ -1,6 +1,6 @@
 package aula07;
 
-public class DateYMD {
+public class DateYMD extends Date {
 
     private int day;
     private int month;
@@ -36,40 +36,6 @@ public class DateYMD {
 
     public int getYear() {
         return this.year;
-    }
-
-    private static boolean validMonth(int month) {
-        if (month >= 1 && month <= 12) {
-            return true;
-        }
-        return false;
-    }
-
-    public static int monthDays(int month, int year) {
-        int totalMonthDays;
-        if (month == 4 || month == 6 || month == 9 || month == 11) {
-            totalMonthDays = 30;
-        } else if (month == 2) {
-            totalMonthDays = leapYear(year) ? 29 : 28;
-        } else {
-            totalMonthDays = 31;
-        }
-        return totalMonthDays;
-    }
-
-    private static boolean leapYear(int year) {
-        if (year % 4 == 0) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean valid(int day, int month, int year) {
-        if (day > monthDays(month, year) || day <= 0
-                || !validMonth(month) || year < 1000) {
-            return false;
-        }
-        return true;
     }
 
     public void increment() {
