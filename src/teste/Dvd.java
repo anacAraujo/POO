7f4ff080a5837dfc88f5125c6b08d165e50e29f5
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-04-06 23:32:23 +0100

    Fix Dvd

--- src/teste/Dvd.java
+++ src/teste/Dvd.java
@@ -7,4 +7,37 @@ public class Dvd extends LibraryItem {
         super(titulo);
         this.duracao = d;
     }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + duracao;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Dvd other = (Dvd) obj;
+        if (duracao != other.duracao)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, Duraçao: %d",
+                super.toString(),
+                this.duracao);
+    }
 }
