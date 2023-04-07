fbdf28adf379ce6eeea7386000adc95d285e9483
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-04-07 16:06:03 +0100

    Create class

---
+++ src/aula08/Automovel.java
@@ -0,0 +1,28 @@
+package aula08;
+
+public abstract class Automovel {
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private double potencia;
+
+    private Automovel() {
+
+    }
+
+    public void validMaricula() {
+
+    }
+
+    public void validMarca() {
+
+    }
+
+    public void validModelo() {
+
+    }
+
+    public void validPotencia() {
+
+    }
+}
