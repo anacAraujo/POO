95124959250f1700978d45c8b30c9dcbf99b91d0
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-04-10 00:34:53 +0100

    Fix numQuadro

--- src/aula08/Ex01/Pesado.java
+++ src/aula08/Ex01/Pesado.java
@@ -1,10 +1,10 @@
 package aula08.Ex01;
 
 public abstract class Pesado extends Automovel {
-    private int numQuadro;
+    private String numQuadro;
     private double peso;
 
-    public Pesado(String matricula, String marca, String modelo, double potencia, int numQuadro, double peso) {
+    public Pesado(String matricula, String marca, String modelo, double potencia, String numQuadro, double peso) {
         super(matricula, marca, modelo, potencia);
         this.numQuadro = numQuadro;
         this.peso = peso;
@@ -12,7 +12,7 @@ public abstract class Pesado extends Automovel {
 
     @Override
     public String toString() {
-        return String.format("%s; Nº Quadro: %d; Peso: %f",
+        return String.format("%s; Nº Quadro: %s; Peso: %f",
                 super.toString(),
                 this.numQuadro,
                 this.peso);
