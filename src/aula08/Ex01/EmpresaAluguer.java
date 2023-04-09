de15e72fa5e965c02540aa9c89dc721a3249e74b
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-04-10 00:34:07 +0100

    Add toString and adicionarAutomovel

--- src/aula08/Ex01/EmpresaAluguer.java
+++ src/aula08/Ex01/EmpresaAluguer.java
@@ -5,14 +5,31 @@ import java.util.List;
 
 public class EmpresaAluguer {
     private String nome;
-    private int codgPostal;
+    private String codgPostal;
     private String email;
     private List<Automovel> automoveis;
 
-    public EmpresaAluguer(String nome, int codgPostal, String email) {
+    public EmpresaAluguer(String nome, String codgPostal, String email) {
         this.nome = nome;
         this.codgPostal = codgPostal;
         this.email = email;
         this.automoveis = new ArrayList<Automovel>();
+    }
+
+    public void adicionarAutomovel(Automovel automovel) {
+        automoveis.add(automovel);
+    }
+
+    @Override
+    public String toString() {
+        String automoveis = "";
+        for (Automovel automovel : this.automoveis) {
+            automoveis += automovel.toString() + "\n";
+        }
+        return String.format("Empresa %s, Código postal: %s, E-mail: %s, Automóveis:\n %s",
+                this.nome,
+                this.codgPostal,
+                this.email,
+                automoveis);
     }
 }
