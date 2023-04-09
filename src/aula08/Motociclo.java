60143fb3c1cd26448415e274abd71f4ef93d4401
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-04-09 22:44:02 +0100

    Add validation for tipo

--- src/aula08/Motociclo.java
+++ src/aula08/Motociclo.java
@@ -1,11 +1,30 @@
 package aula08;
 
 public class Motociclo extends Automovel {
+    private static String[] tipos = { "desportivo", "estrada" };
+
     private String tipo;
 
     public Motociclo(String matricula, String marca, String modelo, double potencia, String tipo) {
         super(matricula, marca, modelo, potencia);
         this.tipo = tipo;
+    }
+
+    public boolean validTipo(String tipo) {
+        if (tipo != "desportivo" || tipo != "estrada") {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 
     @Override
