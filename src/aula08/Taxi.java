45af39268d7f8f3bc1d03a4a8acaa62d9486c611
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-04-09 22:47:59 +0100

    Add constructor, hashCode, equals and toString

---
+++ src/aula08/Taxi.java
@@ -0,0 +1,28 @@
+package aula08;
+
+public class Taxi extends Ligeiro {
+    private int licenca;
+
+    public Taxi(String matricula, String marca, String modelo, double potencia, int numQuadro, int capacidadeBagageira,
+            int licenca) {
+        super(matricula, marca, modelo, potencia, numQuadro, capacidadeBagageira);
+        this.licenca = licenca;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s; Lincença: %d",
+                super.toString(),
+                this.licenca);
+    }
+}
