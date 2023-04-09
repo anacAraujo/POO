6d31a4a46cdf9fcc045c4071bb39117a9ab6c50b
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-04-09 22:25:14 +0100

    Add constructor and toString

--- src/aula08/Automovel.java
+++ src/aula08/Automovel.java
@@ -6,8 +6,11 @@ public abstract class Automovel {
     private String modelo;
     private double potencia;
 
-    private Automovel() {
-
+    public Automovel(String matricula, String marca, String modelo, double potencia) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.potencia = potencia;
     }
 
     public void validMaricula() {
@@ -24,5 +27,14 @@ public abstract class Automovel {
 
     public void validPotencia() {
 
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Matricula: %s; Marca: %s; Modelo: %s; Potencia: %d",
+                this.matricula,
+                this.marca,
+                this.modelo,
+                this.potencia);
     }
 }
