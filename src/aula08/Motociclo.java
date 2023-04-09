af79633e632176d6af5fec401957bbd3f007c8f4
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-04-09 22:28:19 +0100

    Add constructor and toString

--- src/aula08/Motociclo.java
+++ src/aula08/Motociclo.java
@@ -3,7 +3,15 @@ package aula08;
 public class Motociclo extends Automovel {
     private String tipo;
 
-    public Motociclo(String matricula, String marca, String modelo, double potencia) {
-        super();
+    public Motociclo(String matricula, String marca, String modelo, double potencia, String tipo) {
+        super(matricula, marca, modelo, potencia);
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s; Tipo: %s;",
+                super.toString(),
+                this.tipo);
     }
 }
