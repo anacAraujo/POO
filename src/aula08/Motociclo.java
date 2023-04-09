ac233168e53efe184199e4153f6de2eca2c9652e
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-04-09 22:25:28 +0100

    Create class

---
+++ src/aula08/Motociclo.java
@@ -0,0 +1,9 @@
+package aula08;
+
+public class Motociclo extends Automovel {
+    private String tipo;
+
+    public Motociclo(String matricula, String marca, String modelo, double potencia) {
+        super();
+    }
+}
