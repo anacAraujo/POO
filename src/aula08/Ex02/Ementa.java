90e676150e41b9cfc6b27ffb501665b40650995c
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-04-12 21:19:58 +0100

    Create class

---
+++ src/aula08/Ex02/Ementa.java
@@ -0,0 +1,35 @@
+package aula08.Ex02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Ementa {
+    public Map<int,String>DiaSemana=
+    { "Segunda", "Terça", "Quarta", "Quinta", "Sexta" };
+
+    private String nome;
+    private String local;
+    private Map<Prato, String> pratos;
+
+    public Ementa(String nome, String local) {
+        this.nome = nome;
+        this.local = local;
+        pratos = new HashMap<>();
+    }
+
+    public void addPrato(Prato prato, String dia) {
+        pratos.entrySet();
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (Prato prato : pratos) {
+            result = prato.toString() + "\n";
+        }
+        return result;
+    }
+}
