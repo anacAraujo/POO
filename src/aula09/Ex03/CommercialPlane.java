2ab534020835ae82ffe0ca94e757cbde88672b24
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-04-19 15:44:55 +0100

    Add methods

--- src/aula09/Ex03/CommercialPlane.java
+++ src/aula09/Ex03/CommercialPlane.java
@@ -1,5 +1,21 @@
 package aula09.Ex03;
 
-public class CommercialPlane {
+public class CommercialPlane extends Plane {
+    private int tripulantes;
 
+    public CommercialPlane(String nome, String fab, int ano, int maxPassageiros, int maxVelocidade, int tripulantes) {
+        super(nome, fab, ano, maxPassageiros, maxVelocidade);
+        this.tripulantes = tripulantes;
+    }
+
+    public String getPlaneType() {
+        return "Comercial";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Avião Comercial %s, Nº Tripulantes: %d",
+                super.toString(),
+                this.tripulantes);
+    }
 }
