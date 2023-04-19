fcef7a2f1208839336252862810ce41b641ce8a1
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-04-19 12:51:27 +0100

    Create class

---
+++ src/aula09/Ex03/Plane.java
@@ -0,0 +1,17 @@
+package aula09.Ex03;
+
+public abstract class Plane {
+    private String nome;
+    private String fabricante;
+    private int anoProducao;
+    private int maxPassageiros;
+    private int maxVelocidade;
+
+    public Plane(String nome, String fab, int ano, int maxPassageiros, int maxVelocidade) {
+        this.nome = nome;
+        this.fabricante = fab;
+        this.anoProducao = ano;
+        this.maxPassageiros = maxPassageiros;
+        this.maxVelocidade = maxVelocidade;
+    }
+}
