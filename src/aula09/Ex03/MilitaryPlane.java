2b6dfcec623e81a8064dad7e16883d66799b3333
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-04-19 15:45:06 +0100

    Add methods

--- src/aula09/Ex03/MilitaryPlane.java
+++ src/aula09/Ex03/MilitaryPlane.java
@@ -1,5 +1,21 @@
 package aula09.Ex03;
 
-public class MilitaryPlane {
+public class MilitaryPlane extends Plane {
+    private int municoes;
 
+    public MilitaryPlane(String nome, String fab, int ano, int maxPassageiros, int maxVelocidade, int municoes) {
+        super(nome, fab, ano, maxPassageiros, maxVelocidade);
+        this.municoes = municoes;
+    }
+
+    public String getPlaneType() {
+        return "Militar";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Avião Comercial %s, Nº Municoes: %d",
+                super.toString(),
+                this.municoes);
+    }
 }
