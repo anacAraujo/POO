b7ba85d2f51865bb0cd331b6c96adc49b763ab2f
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-05-06 22:57:27 +0100

    Fix exercice 4

--- src/aula10/ex4/DemoEx4.java
+++ src/aula10/ex4/DemoEx4.java
@@ -4,24 +4,20 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Iterator;
 
 public class DemoEx4 {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(new FileReader("words.txt"));
 
         ArrayList<String> wordsMoreThenTwo = new ArrayList<>();
-        ArrayList<String> wordsEndS = new ArrayList<>();
 
         while (input.hasNext()) {
             String word = input.next();
             System.out.println(word);
 
-            if (word.length() > 2 && word.matches("[a-zA-Z]+")) {
+            if (word.length() > 2) {
                 wordsMoreThenTwo.add(word);
-            }
-
-            if (word.endsWith("s") && word.matches("[a-zA-Z]+")) {
-                wordsEndS.add(word);
             }
         }
 
@@ -31,8 +27,24 @@ public class DemoEx4 {
         }
 
         System.out.println("\nPalavras que acabam em 's':");
-        for (String word : wordsEndS) {
+        for (String word : wordsMoreThenTwo) {
+            if (word.endsWith("s")) {
+                System.out.println(word);
+            }
+        }
+
+        System.out.println("\nRemover palavras com caracteres especiais:");
+        Iterator<String> itr = wordsMoreThenTwo.iterator();
+        while (itr.hasNext()) {
+            String word = itr.next();
+            if (!word.matches("[a-zA-Z]+")) {
+                itr.remove();
+            }
+        }
+
+        for (String word : wordsMoreThenTwo) {
             System.out.println(word);
         }
+
     }
 }
