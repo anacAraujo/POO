941947f69e1fa6ddee5d2821dabf7ed7a6d44378
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-05-10 23:07:24 +0100

    Create file

---
+++ src/aula11/ex3/EnergyUsageReport.java
@@ -0,0 +1,35 @@
+package aula11.ex3;
+
+public class EnergyUsageReport {
+
+    // lê o ficheiro com os dados dos clientes e guarda os valores numa Java
+    // Collection;
+    public void load() {
+
+    }
+
+    // adiciona/remove um cliente;
+    public void addCustomer() {
+
+    }
+
+    public void removeCustomer() {
+
+    }
+
+    // providencia os dados de um determinado cliente;
+    public void getCostumer() {
+
+    }
+
+    // Calcula o uso total de energia de um cliente;
+    public void calculateTotalUsage() {
+
+    }
+
+    // Gera o relatório onde é listado em cada linha o ID do cliente e a energia
+    // consumida.
+    public void generateReport() {
+
+    }
+}
